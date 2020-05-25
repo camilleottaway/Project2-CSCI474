d9df4de667ab2682f574b99daaaf16b5932b89a7
Benjamin <larkinb3@wwu.edu>
2020-05-25 14:03:06 -0700

    wip

--- v3/script.py
+++ v3/script.py
@@ -1,21 +1,17 @@
 import os
 import sys
 import numpy as np
+import subprocess
 
 def main():
-    counter = 0
     f = open("results.txt","a+")
     for rnaught in np.round(np.arange(0.5, 4.5, 1.0), 2): #step size 0.1
-        print(counter)
         for cfr in np.round(np.arange(0.05, 0.55, .05), 2): #step size 0.05
             for dinf in np.arange(8, 13, 1): #step size 1
                 for psev in np.round(np.arange(0.1, 0.81, 0.1), 2): #step size .1
-                    modelcall = "python3 modelV3.py "
-                    modelcall += f"-r0 {rnaught} -cfr {cfr} -dinf {dinf} -psevere {psev}"
-                    # print(modelcall)
-                    f.write(modelcall +"\n")
-                    os.system(modelcall)
-                    counter+=1
+                    modelargs = ["python3 modelV3.py", f"-r0 {rnaught}",  f"-cfr {cfr}", f"-dinf {dinf}", f"-psevere {psev}", "-population 278000"]
+                    f.write(modelargs[0] + modelargs[1] + modelargs[2] + modelargs[3] + modelargs[4] +"\n")
+                    subprocess.run(modelargs)
 
 if __name__ == "__main__":
     main()
