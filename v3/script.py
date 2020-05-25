278b748fe576d5bbdea56badc8b50e1c06a596fe
cameronCJW <cmrnwllc@gmail.com>
2020-05-25 12:09:51 -0700

    Fixed rounding issues

--- v3/script.py
+++ v3/script.py
@@ -5,10 +5,10 @@ import numpy as np
 
 def main():
     f = open("results.txt","a+")
-    for rnaught in np.arange(0.5, 4.5, 1.0): #step size 0.1
-        for cfr in np.arange(0.05, 0.5, .05): #step size 0.05
+    for rnaught in np.round(np.arange(0.5, 4.5, 1.0), 2): #step size 0.1
+        for cfr in np.round(np.arange(0.05, 0.5, .05), 2): #step size 0.05
             for dinf in np.arange(8, 12, 4): #???? 
-                for psev in np.arange(0.1, 0.8, 0.2): # ????
+                for psev in np.round(np.arange(0.1, 0.8, 0.2), 2): # ????
                     modelcall = "python3 modelV3.py "
                     modelcall += f"-r0 {rnaught} -cfr {cfr} -dinf {dinf} -psevere {psev}"
                     print(modelcall)
