54ceaf660d0dd8b91a595bdb5a8068bc918a70a5
benlarkin <benlarkin.usa@gmail.com>
2020-05-25 14:36:46 -0700

    used subprocess call and removed file writing

--- v3/script.py
+++ v3/script.py
@@ -4,14 +4,13 @@ import numpy as np
 import subprocess
 
 def main():
-    f = open("results.txt","a+")
-    for rnaught in np.round(np.arange(0.5, 4.5, 1.0), 2): #step size 0.1
+    for rnaught in np.round(np.arange(0.5, 4.5, 0.1), 2): #step size 0.1
         for cfr in np.round(np.arange(0.05, 0.55, .05), 2): #step size 0.05
             for dinf in np.arange(8, 13, 1): #step size 1
                 for psev in np.round(np.arange(0.1, 0.81, 0.1), 2): #step size .1
-                    modelargs = ["python3 modelV3.py", f"-r0 {rnaught}",  f"-cfr {cfr}", f"-dinf {dinf}", f"-psevere {psev}", "-population 278000"]
-                    f.write(modelargs[0] + modelargs[1] + modelargs[2] + modelargs[3] + modelargs[4] +"\n")
+                    modelargs = ["python3", "modelV3.py", "-R0", str(rnaught), "-CFR", str(cfr), "-dinf", str(dinf), "-PSEVERE", str(psev), "-N",  "278000"]
                     subprocess.run(modelargs)
+                    print(str(modelargs))
 
 if __name__ == "__main__":
     main()
