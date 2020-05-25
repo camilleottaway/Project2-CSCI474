60201071a2434a1413804700f1d05df3ca0c8aa0
benlarkin <benlarkin.usa@gmail.com>
2020-05-25 13:21:13 -0700

    added counter

--- v3/script.py
+++ v3/script.py
@@ -3,8 +3,10 @@ import sys
 import numpy as np
 
 def main():
+    counter = 0
     f = open("results.txt","a+")
     for rnaught in np.round(np.arange(0.5, 4.5, 1.0), 2): #step size 0.1
+        print(counter)
         for cfr in np.round(np.arange(0.05, 0.55, .05), 2): #step size 0.05
             for dinf in np.arange(8, 13, 1): #step size 1
                 for psev in np.round(np.arange(0.1, 0.81, 0.1), 2): #step size .1
@@ -13,6 +15,7 @@ def main():
                     # print(modelcall)
                     f.write(modelcall +"\n")
                     os.system(modelcall)
+                    counter+=1
 
 if __name__ == "__main__":
     main()
