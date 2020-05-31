6cb95a1730f86bc2a1b9c52eab7df4fbb712454c
camilleottaway <camilleottaway@gmail.com>
2020-05-31 14:02:55 -0700

    added csv writing for cross referecing params

--- v3/script.py
+++ v3/script.py
@@ -2,8 +2,15 @@ import os
 import sys
 import numpy as np
 import subprocess
+import csv
 
 def main():
+
+    with open('parameters.csv', 'a+') as csvfile:
+        fieldnames = ['-R0', '-CFR', '-dinf', '-PSEVERE', '-N', '-run' ]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
     run = 0
     for rnaught in np.round(np.arange(0.5, 4.5, 0.1), 2): #step size 0.1
         for cfr in np.round(np.arange(0.05, 0.55, .05), 2): #step size 0.05
@@ -12,7 +19,12 @@ def main():
                     modelargs = ["python3", "modelV3.py", "-R0", str(rnaught), "-CFR", str(cfr), "-dinf", str(dinf), "-PSEVERE", str(psev), "-N",  "278000", "-run", str(run)]
                     subprocess.run(modelargs)
                     print(str(modelargs))
-                    run+=1
+                    with open('parameters.csv', 'a+') as csvfile:
+                        fieldnames = ['-R0', '-CFR', '-dinf', '-PSEVERE', '-N', '-run' ]
+                        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                        params = {'-R0': rnaught, '-CFR': cfr, '-dinf': dinf, '-PSEVERE': psev, '-N':  278000, '-run': run}
+                        writer.writerow(params)
+                    run += 1
 
 if __name__ == "__main__":
     main()
--- v3/scriptR0.py
+++ v3/scriptR0.py
@@ -1,9 +1,24 @@
 import os
 import sys
+import csv
 import numpy as np
 import subprocess
 
+def f(defaultValues):
+    
+    R0FilePath = defaultValues["R0FilePath"] 
+    if (UseDecayingR0):
+        arrayOfR0s = GetR0DecayValues(R0FilePath)
+        if (arrayOfR0s == None):
+            exit()
+
 def main():
+
+    with open('parametersR0.csv', 'w') as csvfile:
+        fieldnames = ['R0', 'CFR', 'dinf', 'PSEVERE', 'N', 'run' ]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
     run = 0
     newpath = os.getcwd()
     print(newpath)
@@ -14,7 +29,10 @@ def main():
                 for psev in np.round(np.arange(0.1, 0.81, 0.1), 2): #step size .1
                     modelargs = ["python3", "modelV3.py", "-decay", f"R{rnaught}.txt", "-CFR", str(cfr), "-dinf", str(dinf), "-PSEVERE", str(psev), "-N",  "278000", "-run", str(run)]
                     subprocess.run(modelargs)
-                    print(str(modelargs))
+                    with open('parametersR0.csv', 'a+') as csvfile:
+                        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                        params = {'R0': f"R{rnaught}.txt", 'CFR': cfr, 'dinf': dinf, 'PSEVERE': psev, 'N':  278000, 'run': run}
+                        writer.writerow(params)
                     run+=1
 
 if __name__ == "__main__":
