a1bf06fc0449aaae3b258a5634c3a0e92371028d
claeysg <claeysg@wwu.edu>
2020-06-04 13:15:36 -0700

    outputs a sumamry of the parameters for the worst runs

--- dataparser/dataparser.py
+++ dataparser/dataparser.py
@@ -7,24 +7,60 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 
-model_df = pd.read_csv("results.csv")
-params_df= pd.read_csv("parametersR0.csv")
+#model_df = pd.read_csv("resultsR0Decay.csv")
+#model_df = pd.read_csv("resultsR0Only.csv")
+model_df = pd.read_csv("resultsR0Decay.csv")
+#params_df= pd.read_csv("parametersR0.csv")
+#params_df= pd.read_csv("parametersR0Only.csv")
+params_df= pd.read_csv("parametersR0Decay.csv")
+params_df = params_df.apply(pd.to_numeric, errors='coerce')
 infection_df = model_df[['Infected','Run']]
 infection_df = infection_df.apply(pd.to_numeric, errors='coerce')
 max_infected_perRun = infection_df.groupby('Run')['Infected'].max()
+max_infected_perRun = max_infected_perRun.apply(pd.to_numeric, errors='coerce')
 
 #infection_df.groupby('Run')['Infected'].max().plot.box()
 #infection_df.groupby('Run')['Infected'].max().plot.hist(bins=30)
-#infection_df.groupby('Run')['Infected'].max().plot.hist(bins=30)
 
-#time_series_df = model_df[['Infected','Run','Time']]
-#first_chunk = time_series_df.iloc[:600]
-#first_chunk['Time'] = pd.to_datetime(first_chunk['Time'])
-first_chunk = pd.DataFrame(first_chunk['Infected'],columns=first_chunk['Run'])
-#first_chunk.plot()
-#time_series_df.plot()
+Q1=max_infected_perRun.quantile(0.25)
+Q3=max_infected_perRun.quantile(0.75)
+IQR = Q3 - Q1
+max = Q3 + 1.5* IQR
+
+biggest_infections = max_infected_perRun[max_infected_perRun.between(max, max_infected_perRun.quantile(0.99))]
+biggest_infections = biggest_infections.to_frame()
+worstR0 = biggest_infections.join(params_df.set_index('Run'))
+R0_dups = worstR0['R0'].to_list()
+R0s = []
+for i in R0_dups:
+    R0s.append(i)
+    while i in R0_dups:
+        R0_dups.remove(i)
+#print(R0s)
+
+worstR0Decays = []
+for i in R0s:
+    file = open('../v3/R'+str(i)+".txt", 'r')
+    worstR0Decays.append(file.read().replace('\n',' '))
+
+print("Social Distancing R0's for simulations above the 3rd quartile:")
+print()
+for i in worstR0Decays:
+    print(i)
+
+print()
+print("Range of parameters for worst runs: ")
+print("Infected: " + str(worstR0['Infected'].min()) + " - " + str(worstR0['Infected'].max()))
+print("CFR: " + str(worstR0['CFR'].min()) + " - " +  str(worstR0['CFR'].max()))
+print("dinf: " + str(worstR0['dinf'].min()) + " - " +  str(worstR0['dinf'].max()))
+print("PSEVERE: " + str(worstR0['PSEVERE'].min()) + " - " +  str(worstR0['PSEVERE'].max()))
+print("Number: " + str(worstR0['N'].min()) + " - " +  str(worstR0['N'].max()))
 
 
-infection_df.groupby('Run')['Infected'].max().plot.box()
+
+#max_infected_perRun.plot.box()
+#plt.show()
+
+#infection_df.groupby('Run')['Infected'].max().plot.box()
+#print(max_infected_perRun.summary())
 #print(infection_df.groupby('Run')['Infected'].max())
-plt.show()
