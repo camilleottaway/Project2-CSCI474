0ee91385c5f279901b1bfbd7aa90c122d5a172ae
camilleottaway <camilleottaway@gmail.com>
2020-06-04 18:22:25 -0700

    uploaded visulaizations

--- dataparser/dataparser.py
+++ dataparser/dataparser.py
@@ -6,55 +6,50 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
+# Read in results and parameters for all varied parameters as dataframes
+model_df = pd.read_csv("resultsR0Decay.csv")
+params_df= pd.read_csv("parametersR0Decay.csv")
+infection_df = model_df[['Infected','Run']]
+infection_df = infection_df.apply(pd.to_numeric, errors='coerce')
+max_infected_perRun = infection_df.groupby('Run')['Infected'].max()
 
-# model_df = pd.read_csv("results.csv")
-# params_df= pd.read_csv("parametersR0.csv")
-# infection_df = model_df[['Infected','Run']]
-# infection_df = infection_df.apply(pd.to_numeric, errors='coerce')
-# max_infected_perRun = infection_df.groupby('Run')['Infected'].max()
+#boxplot of max from each run
+infection_df.groupby('Run')['Infected'].max().plot.box()
+plt.show()
 
-#infection_df.groupby('Run')['Infected'].max().plot.box()
-#infection_df.groupby('Run')['Infected'].max().plot.hist(bins=30)
-#infection_df.groupby('Run')['Infected'].max().plot.hist(bins=30)
-
-#time_series_df = model_df[['Infected','Run','Time']]
-#first_chunk = time_series_df.iloc[:600]
-#first_chunk['Time'] = pd.to_datetime(first_chunk['Time'])
-# first_chunk = pd.DataFrame(first_chunk['Infected'],columns=first_chunk['Run'])
-# #first_chunk.plot()
-# #time_series_df.plot()
-
-
-# infection_df.groupby('Run')['Infected'].max().plot.box()
-# #print(infection_df.groupby('Run')['Infected'].max())
-# plt.show()
+#histogram of max from each run
+infection_df.groupby('Run')['Infected'].max().plot.hist(bins=30)
+plt.show()
 
 
 
 #Analyzing R0 Only
+
+#read in data and crate dataframes
 modelR0Only_df = pd.read_csv("resultsR0Only.csv")
 paramsR0Only_df= pd.read_csv("parametersR0Only.csv")
 infectionR0Only_df = modelR0Only_df[['Infected','Time','Run']]
 
+#convert time to date
 infectionR0Only_df['Time'] = infectionR0Only_df['Time'].apply(lambda x: str(x).split(' ')[0])
 pd.to_datetime(infectionR0Only_df['Time'], format ='%Y-%m-%d')
-##print(infectionR0Only_df.head)
-#print(infectionR0Only_df.groupby('Run').groups) #= pd.DataFrame(infectionR0Only_df['Infected'],columns=infectionR0Only_df['Run'])columns=infectionR0Only_df.groupby('Run').groups
+#print(infectionR0Only_df.head)
+#print(infectionR0Only_df.groupby('Run').groups)
+
+#Tried to get R0 values into dataframe
+# combined_df = infectionR0Only_df.join(paramsR0Only_df.set_index('Run'))
+# combined_df = combined_df[['Infected','Time','Run', 'R0']]
+# print(combined_df)
+# R0Decays = []
+# for i in range(0,51):
+#     file = open('../v3/R'+str(i)+".txt", 'r')
+#     R0Decays.append(file.read().replace('\n',' '))
+# print(R0Decays)
 
 
-# timeseries_df =pd.DataFrame(infectionR0Only_df['Infected'], index= pd.date_range('2020-01-15', periods=300))
-# print(timeseries_df)
-
-combined_df = infectionR0Only_df.join(paramsR0Only_df.set_index('Run'))
-print(combined_df)
-worstR0Decays = []
-for i in range(0,51):
-    file = open('../v3/R'+str(i)+".txt", 'r')
-    worstR0Decays.append(file.read().replace('\n',' '))
-
-
-# infectionR0Only_df.pivot_table(index='Time',columns='Run',values='Infected').plot()
-# print(infectionR0Only_df.pivot_table(index='Time',columns='Run',values='Infected'))
-# #infectionR0Only_df.plot()
-# plt.show()
+#Plot time series of each run
+infectionR0Only_df.pivot_table(index='Time',columns='Run',values='Infected').plot()
+#print(infectionR0Only_df.pivot_table(index='Time',columns='Run',values='Infected'))
+#infectionR0Only_df.plot()
+plt.show()
 
